61b8b69d2650265a50eea5e0e42e76efd8160e5a
Ali Rashidi <aliinreallifee@gmail.com>
2024-06-02 12:32:34 +0330

    add dynamic collection namings

--- src/keeper/keeper.py
+++ src/keeper/keeper.py
@@ -1,34 +1,36 @@
 from pymongo import MongoClient
 from typing import Dict, Any
 
+# Create a connection to the MongoDB database
+client = MongoClient("mongodb://localhost:27017/")
 
-def insert_data(data: Dict[str, Any]) -> bool:
+# Access the database
+db = client["real_estate_data"]
+
+
+def insert_data(data: Dict[str, Any], collection_name) -> bool:
     """
     Given a dictionary of data, this function inserts the data into a MongoDB database.
 
     Args:
         data (dict): A dictionary containing the data to insert.
+    """
+    # Access the collection
+    collection = db[collection_name]
+
+    collection.insert_one(data)
+
+
+def is_id_in_database(id, collection_name) -> bool:
+    """this function checks if a document with the given id exists in the database
+    Args:
+        id (str): the id of the document
+        collection_name (str): the name of the collection
 
     Returns:
-        bool: True if the data was inserted successfully, False otherwise.
+        bool: True if the document exists, False otherwise
     """
-    # Create a connection to the MongoDB database
-    client = MongoClient("mongodb://localhost:27017/")
-
-    # Access the database
-    db = client["real_estate_data"]
-
     # Access the collection
-    collection = db["listings"]
-
-    # Check if a document with the same link already exists
-    existing_document = collection.find_one({"link": data["link"]})
-
-    if existing_document is None:
-        # Insert the data into the collection
-        collection.insert_one(data)
-        print("Data inserted successfully")
-        return True
-    else:
-        print("Data already exists in the database")
-        return False
+    collection = db[collection_name]
+    existing_document = collection.find_one({"_id": id})
+    return existing_document is not None
