31556905dcc4cf6893b3a9dfa9c48ac42e84ac1b
Ali Rashidi <aliinreallifee@gmail.com>
2024-06-05 17:33:56 +0330

    make inhancments

--- src/crawler.py
+++ src/crawler.py
@@ -48,7 +48,6 @@ def extract_IDs(url: str) -> List[str]:
     soup = BeautifulSoup(response.content, "html.parser")
     parent_divs = soup.find_all("div", class_=PARENT_CLASS)
     if not parent_divs:
-        print("No parent divs found")
         return []
 
     IDs = []
@@ -74,7 +73,6 @@ def extract_real_estate_data(id: str) -> Optional[Dict[str, any]]:
     url = BASE_URL + id
     response = requests.get(url)
     if response.status_code != 200:
-        print(f"Failed to load webpage: {url}")
         return None
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -82,70 +80,67 @@ def extract_real_estate_data(id: str) -> Optional[Dict[str, any]]:
 
     # Extract the title
     title = soup.find("div", class_=TITLE_CLASS)
-    if title:
+    if title:  # if we dont find title than there is somthing wrong with soup
         data["title"] = title.text.strip()
 
-    # Extract the price
-    price = soup.find("p", class_=PRICE_CLASS)
-    if price:
-        try:
-            price_text = price.text.replace("٬", "").replace("تومان", "").strip()
-            data["price"] = int(price_text)
-        except ValueError:
-            data["price"] = "Invalid price"
-
-    # Extract property details
-    data_rows = soup.find_all("tr", class_=DATA_ROW_CLASS)
-    if data_rows:
-        items = data_rows[0].find_all("td", class_=GROUP_ROW_ITEM_CLASS)
-        if len(items) == 3:
+        # Extract the price
+        price = soup.find("p", class_=PRICE_CLASS)
+        if price:
             try:
-                data["metrage"] = int(items[0].text.strip())
-                year_text = items[1].text.strip()
-                data["year_of_construction"] = (
-                    int(year_text) if year_text.isdigit() else "Before 1370"
-                )
-                data["number_of_rooms"] = int(items[2].text.strip())
+                price_text = price.text.replace("٬", "").replace("تومان", "").strip()
+                data["price"] = int(price_text)
             except ValueError:
-                data["metrage"] = data["year_of_construction"] = data[
-                    "number_of_rooms"
-                ] = "Invalid data"
+                data["price"] = "Invalid price"
 
-        if len(data_rows) > 1:
-            items = data_rows[1].find_all("td", class_=GROUP_ROW_ITEM_CLASS)
+        # Extract property details
+        data_rows = soup.find_all("tr", class_=DATA_ROW_CLASS)
+        if data_rows:
+            items = data_rows[0].find_all("td", class_=GROUP_ROW_ITEM_CLASS)
             if len(items) == 3:
-                data["has_elevator"] = "ندارد" not in items[0].text
-                data["has_parking"] = "ندارد" not in items[1].text
-                data["has_storage_room"] = "ندارد" not in items[2].text
+                try:
+                    data["metrage"] = int(items[0].text.strip())
+                    year_text = items[1].text.strip()
+                    data["year_of_construction"] = (
+                        int(year_text) if year_text.isdigit() else "Before 1370"
+                    )
+                    data["number_of_rooms"] = int(items[2].text.strip())
+                except ValueError:
+                    data["metrage"] = data["year_of_construction"] = data[
+                        "number_of_rooms"
+                    ] = "Invalid data"
 
-    # Extract agency and agent details
-    agency = soup.find("a", class_=AGENCY_CLASS)
-    if agency:
-        data["agency"] = agency.text.strip()
-        agency.decompose()
-    agent = soup.find("a", class_=AGENCY_CLASS)
-    if agent:
-        data["agent"] = agent.text.strip()
+            if len(data_rows) > 1:
+                items = data_rows[1].find_all("td", class_=GROUP_ROW_ITEM_CLASS)
+                if len(items) == 3:
+                    data["has_elevator"] = "ندارد" not in items[0].text
+                    data["has_parking"] = "ندارد" not in items[1].text
+                    data["has_storage_room"] = "ندارد" not in items[2].text
 
-    # Extract image presence
-    image = soup.find("img", class_=IMAGE_CLASS)
-    if data:
-        data["has_image"] = bool(
-            image
-            and "ls-is-cached" not in image.get("class")
-            and "kt-image-block__image--lazy-loaded" not in image.get("class")
-        )
+        # Extract agency and agent details
+        agency = soup.find("a", class_=AGENCY_CLASS)
+        if agency:
+            data["agency"] = agency.text.strip()
+            agency.decompose()
+        agent = soup.find("a", class_=AGENCY_CLASS)
+        if agent:
+            data["agent"] = agent.text.strip()
 
-    # Extract the description
-    description = soup.find("p", class_=DESCRIPTION_CLASS)
-    if description:
-        data["description"] = (
-            description.text.strip() if description else "No description available"
-        )
+        # Extract image presence
+        image = soup.find("img", class_=IMAGE_CLASS)
+        if data:
+            data["has_image"] = bool(
+                image
+                and "ls-is-cached" not in image.get("class")
+                and "kt-image-block__image--lazy-loaded" not in image.get("class")
+            )
 
-    if data:
+        # Extract the description
+        description = soup.find("p", class_=DESCRIPTION_CLASS)
+        if description:
+            data["description"] = (
+                description.text.strip() if description else "No description available"
+            )
+
         data["_id"] = id
-    else:
-        print(f"Failed to extract data from {url}")
 
     return data
