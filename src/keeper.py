5dcb3faed82041eb191c7cf9b03af55e7e68c501
Ali Rashidi <aliinreallifee@gmail.com>
2024-06-05 13:51:23 +0330

    add online server/home/ali/miniconda3/envs/persian-digit-recognition-dev/bin/python /home/ali/divar-digger/src/main.py

--- src/keeper.py
+++ src/keeper.py
@@ -7,13 +7,21 @@ from pymongo import MongoClient
 load_dotenv()
 
 mongodb_host_port = getenv("MONGODB_HOST_PORT")
-mongodb_connection_string = f"mongodb://{mongodb_host_port}/"
+mongodb_local_connection_string = f"mongodb://{mongodb_host_port}/"
 
-# Create a connection to the MongoDB database
-client = MongoClient(mongodb_connection_string)
+# Create a connection to the local MongoDB database
+local_client = MongoClient(mongodb_local_connection_string)
 
-# Access the database
-db = client["real_estate_data"]  # TODO: make it more flexible
+# Access the local database
+local_db = local_client["real_estate_data"]  # TODO: make it more flexible
+
+mongodb_online_connection_string = getenv("MONGODB_ONLINE_CONNECTION_STRING")
+
+# Create a connection to the online MongoDB database
+online_client = MongoClient(mongodb_online_connection_string)
+
+# Access the online database
+online_db = online_client["real_estate_data"]  # TODO: make it more flexible
 
 
 def insert_data(data: Dict[str, Any], collection_name) -> bool:
@@ -23,10 +31,13 @@ def insert_data(data: Dict[str, Any], collection_name) -> bool:
     Args:
         data (dict): A dictionary containing the data to insert.
     """
-    # Access the collection
-    collection = db[collection_name]
+    # Access the local and online collections
+    local_collection = local_db[collection_name]
+    online_collection = online_db[collection_name]
 
-    collection.insert_one(data)
+    # Insert the data into the local and online collections
+    local_collection.insert_one(data)
+    online_collection.insert_one(data)
 
 
 def is_id_in_database(id, collection_name) -> bool:
@@ -39,6 +50,6 @@ def is_id_in_database(id, collection_name) -> bool:
         bool: True if the document exists, False otherwise
     """
     # Access the collection
-    collection = db[collection_name]
-    existing_document = collection.find_one({"_id": id})
+    local_collection = local_db[collection_name]
+    existing_document = local_collection.find_one({"_id": id})
     return existing_document is not None
